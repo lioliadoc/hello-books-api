5fa00b6c0942985ccd89bdf43808a2a638a09a5c
lioliadoc <liolia.doc@gmail.com>
2024-10-24 20:15:54 -0700

    book

--- app/models/books.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from ..db import db
-
-class Book(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str]
-    description: Mapped[str]
